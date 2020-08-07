41333938183342158b6622c41c75878b9e28b3a0
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-08-07 11:09:21 +0900

    Create 51_N-Queens.cc

---
+++ LeetCode/51_N-Queens.cc
@@ -0,0 +1,43 @@
+
+
+
+/* answer1: recusion */
+class Solution {
+public:
+    std::vector<std::vector<std::string> > solveNQueens(int n) {
+        std::vector<std::vector<std::string> > res;
+        std::vector<std::string> nQueens(n, std::string(n, '.'));
+        solveNQueens(res, nQueens, 0, n);
+        return res;
+    }
+private:
+    void solveNQueens(std::vector<std::vector<std::string> > &res, std::vector<std::string> &nQueens, int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (isValid(nQueens, row, col, n)) {
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, row + 1, n);
+                nQueens[row][col] = '.';
+            }
+    }
+    bool isValid(std::vector<std::string> &nQueens, int row, int col, int &n) {
+        //check if the column had a queen before.
+        for (int i = 0; i != row; ++i)
+            if (nQueens[i][col] == 'Q')
+                return false;
+        
+        for (int i = 0; i != row; i++){
+            for (int j = 0; j < n; j++){
+                int rowDistance = row - i;
+                int colDistance = abs(j - col);
+                if (nQueens[i][j] == 'Q' && rowDistance == colDistance){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
