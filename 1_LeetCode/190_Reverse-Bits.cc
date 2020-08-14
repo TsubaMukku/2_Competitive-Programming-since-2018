4494649789788701955ae832444fad1347d995d3
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-08-14 15:04:06 +0900

    Update 190_Reverse-Bits.cc

--- 1_LeetCode/190_Reverse-Bits.cc
+++ 1_LeetCode/190_Reverse-Bits.cc
@@ -26,3 +26,26 @@ public:
     }
 };
 
+
+/*
+    solution2: mask bit
+    
+    1. 32bitを16bitブロック*2へ分割する -> 前後を入れ替える
+    2. 16bitを8bitブロック*2へ分割する -> 前後を入れ替える
+    3. 繰り返す.
+    4. 1bitのブロックになったら, 終了する
+    
+*/
+
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+};
