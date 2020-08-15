a6a6066e325a72d2faca2618573bea98369170f5
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-08-15 11:55:43 +0900

    Update 275_H-Index-II.cc

--- 1_LeetCode/275_H-Index-II.cc
+++ 1_LeetCode/275_H-Index-II.cc
@@ -39,3 +39,26 @@ public:
         return n - left;
     }
 };
+
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int index = 0, n = (int)citations.size();
+        
+        
+        // left, rightのお気持ちがわからない・・・leftとrightはそれぞれ何を意味してるの・・
+        int left = 0;
+        int right = n- 1;
+        
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            
+            if (citations[mid] == n - mid) return n - mid;
+            else if (citations[mid] < n - mid) left = mid + 1;
+            else right = mid - 1;
+        }
+        
+        return n - left;
+    }
+};
