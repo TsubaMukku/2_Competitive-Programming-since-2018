8fc44f4688d099ccc514c66c292d3450aeee8eb6
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-08-24 09:15:19 +0900

    Update 391_Perfect-Rectangle.cc

--- 1_LeetCode/391_Perfect-Rectangle.cc
+++ 1_LeetCode/391_Perfect-Rectangle.cc
@@ -1,3 +1,36 @@
+class Solution {
+public:
+    bool isRectangleCover(vector<vector<int>>& rectangles) {
+        
+        set<pair<int,int>> corners;
+        int area = 0;
+        
+        for (auto rectangle : rectangles){
+            
+            pair<int,int> p1(rectangle[0],rectangle[1]);
+            pair<int,int> p2(rectangle[2],rectangle[3]);
+            pair<int,int> p3(rectangle[0],rectangle[3]);
+            pair<int,int> p4(rectangle[2],rectangle[1]);
+            
+            for ( auto &p : {p1,p2,p3,p4} ){
+                const auto& ret = corners.insert(p);
+                if (!ret.second) corners.erase(ret.first);
+            }
+            
+            area += (p2.first - p1.first) * (p2.second - p1.second);
+        }
+        
+        if (corners.size() != 4) return false;
+        
+        auto &point1 = *begin(corners);
+        auto &point2 = *rbegin(corners);
+        
+        return area == (point2.first - point1.first) * (point2.second - point1.second);
+        
+    }
+};
+
+
 
 
 
