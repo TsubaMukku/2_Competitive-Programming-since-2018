b668571c3229b65653c7dc5ec15e760ecf08e34f
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-08-28 17:14:50 +0900

    Update 50_Pow.cc

--- 1_LeetCode/50_Pow.cc
+++ 1_LeetCode/50_Pow.cc
@@ -54,3 +54,34 @@ public:
         return res;
     }
 };
+
+
+/*
+  solution3: recursive 
+*/
+
+class Solution {
+private:
+    double myPow(double x, long long n){
+        if (n == 0) return 1.0;
+        
+        long long index = n;
+        
+        if (n < 0){
+            x = 1/x;
+            index = -index;
+        }
+        
+        double res = myPow(x,index/2);
+        
+        if (n % 2 == 0) return res*res;
+        else return res*res*x;
+    }
+public:
+    double myPow(double x, int n) {
+        long long index = n;
+        
+        return myPow(x,index);
+        
+    }
+};
