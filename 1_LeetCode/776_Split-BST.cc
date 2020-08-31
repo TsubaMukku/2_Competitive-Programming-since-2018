fea6b0e00e46289b90a21631edecdd4fdde73131
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-01 08:30:49 +0900

    Create 776_Split-BST.cc

---
+++ 1_LeetCode/776_Split-BST.cc
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> splitBST(TreeNode* root, int V) {
+        
+        vector<TreeNode*> res(2,nullptr);
+        if (root == nullptr) return res;
+        
+        if (V < root->val){
+            res[1] = root;
+            auto res1 = splitBST(root->left,V);
+            root->left = res1[1];
+            res[0] = res1[0];
+        }
+        else{
+            res[0] = root;
+            auto res1 = splitBST(root->right,V);
+            root->right = res1[0];
+            res[1] = res1[1];
+        }
+        
+        return res;
+    }
+};
