1c5bbfd1f1e4dfd1c2ff01158e04dee5bab08e58
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-04 08:14:54 +0900

    Create 459_Repeated-Substring-Pattern.cc

---
+++ 1_LeetCode/459_Repeated-Substring-Pattern.cc
@@ -0,0 +1,13 @@
+
+/*
+solution1 : ひらめき
+
+*/
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        
+        return (s+s).substr(1,2*s.size()-2).find(s) != string::npos;
+    }
+};
