0fe04c248d6f57430949b5895ddd6b538ee0071e
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-05 15:04:03 +0900

    Create String-repeat.py

---
+++ 9_Codewars/String-repeat.py
@@ -0,0 +1,10 @@
+def repeat_str(repeat, string):
+    x = ''
+    for i in range(repeat):
+        x += string
+    return x
+    
+   
+   
+def repeat_str(repeat, string):
+    return string * repeat # 文字列を指定した回数繰り返して新しい文字列として取得する-> *を使え!
