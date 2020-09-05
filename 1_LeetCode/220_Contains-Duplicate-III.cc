3b713bbb540fb87d5e54bc52777905976dada2aa
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-05 14:10:41 +0900

    Update 220_Contains-Duplicate-III.cc

--- 1_LeetCode/220_Contains-Duplicate-III.cc
+++ 1_LeetCode/220_Contains-Duplicate-III.cc
@@ -1,24 +1,38 @@
+/*
+固定長の幅の要素だけを対象に最大値と最小値を持っておきたい,
+というのが本来の目的ですが,
+sliding windowで値を1つ追加して1つ削除する,
+という操作を繰り返す必要があるため,
+ordered setがあると楽に実装できます
+
+今回は
+・random accessが必要ない
+・データの追加と削除が, 動的に何度も行われる
+・sort列に対するbinary searchを何度も行いたい
+という状況下であるため, vectorよりもsetを用いるべきである.
+*/
+
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         
-        set<long long> window; 
+        set<long long> st;
         
         for (int i = 0; i < nums.size(); i++){
             
-            // keep the set containing nums i & j at most distance k
-            if (i > k) window.erase( nums[i-k-1] );
+            if (i > k) st.erase( nums[i-k-1] );
             
-            // -t <= x - nums[i]  <= t;
-            // nums[i] - t <= x -> -t <= x - nums[i] 
-            auto pos = window.lower_bound( (long long)nums[i] - (long long)t);
             
-            // x - nums[i] <= tか否かをチェックすれば良い
-            if (pos != window.end() && (long long)*pos - (long long)nums[i] <= (long long)t) return true;
+            // -t <= x - nums[i]
+            auto pos = st.lower_bound( (long long)nums[i] - (long long)t );
             
-            window.insert( nums[i] );
+            // x - nums[i] <= t ???
+            if (pos != st.end() && (long long)*pos - (long long) nums[i] <= (long long)t ) return true;
+            
+            st.insert( nums[i] );
         }
         
+        // すべての検索に引っかからないということは, 条件を満たさないことを意味する.
         return false;
     }
 };
