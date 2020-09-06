a7c5fa0d845f8a086d07210200a125e12fccaa2b
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-06 09:52:26 +0900

    Update Single-character-palindromes.cc

--- 9_Codewars/Single-character-palindromes.cc
+++ 9_Codewars/Single-character-palindromes.cc
@@ -23,3 +23,30 @@ std::string cleanString(const std::string &s) {
   reverse(ans.begin(),ans.end());
   return ans;
 }
+
+
+std::string solve(std::string s){
+  
+  
+  int start = 0, last = s.length()-1, temp_i, temp_j, cnt = 0; 
+  
+  
+  while (start <= last) {
+    temp_i = start, temp_j = last; 
+    
+    
+    if (s[start] == s[last]) start++, last--; 
+    else if (s[start] != s[last] && s[start+1] == s[last]){
+      start++, cnt++; 
+    } 
+    else if (s[start] != s[last] && s[start] == s[last-1]){
+      last--, cnt++; 
+    } 
+      
+    if ((temp_i == start && temp_j == last) || cnt >= 2) return "not possible"; 
+  }
+  
+  if (cnt == 0) return "OK";
+  else return "remove one";
+  
+}
