ffc657c421fed21f8049161a166dd4c3d089716e
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-08 19:38:59 +0900

    Update 1022_Sum-of-Root-To-Leaf-Binary-Numbers.cc

--- 1_LeetCode/1022_Sum-of-Root-To-Leaf-Binary-Numbers.cc
+++ 1_LeetCode/1022_Sum-of-Root-To-Leaf-Binary-Numbers.cc
@@ -60,3 +60,60 @@ Output:
 Expected:
 22
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+2020-09-08 19:38
+以下のように書くと, 処理が動く
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+public:
+    int sumRootToLeaf(TreeNode* root, int val = 0) {
+        
+        if ( root == nullptr) return 0;
+
+        val = val * 2 + root->val;
+        
+        if ( root->left == nullptr && root->right == nullptr ) return val;
+        else return sumRootToLeaf( root->left, val ) + sumRootToLeaf( root->right, val) ;
+       
+    }
+};
