669ea4b642f8258fda0a6ad84db321e6378f59e0
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-11 08:39:29 +0900

    Update 763_Partition-Labels.cc

--- 1_LeetCode/763_Partition-Labels.cc
+++ 1_LeetCode/763_Partition-Labels.cc
@@ -1,3 +1,35 @@
+/*
+    2020-09-11
+*/
+
+
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        vector<int> last(26);
+        
+        for (int i = 0; i < S.size(); i++){
+            last[S[i] - 'a'] = i;
+        }
+        
+        int j = 0, anchor = 0;
+        vector<int> ans;
+        
+        for (int i = 0; i < S.size(); i++){
+            j = max(j,last[S[i] - 'a']);
+            
+            if (i == j){
+                ans.emplace_back(j - anchor+1);
+                anchor = i + 1;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+
+
 class Solution {
 public:
     vector<int> partitionLabels(string S) {
