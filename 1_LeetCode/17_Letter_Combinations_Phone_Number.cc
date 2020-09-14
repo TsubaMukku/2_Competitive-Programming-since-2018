1be79312eff951b89f6308e99cf969c5d13b491d
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-14 10:19:29 +0900

    Update 17_Letter_Combinations_Phone_Number.cc

--- 1_LeetCode/17_Letter_Combinations_Phone_Number.cc
+++ 1_LeetCode/17_Letter_Combinations_Phone_Number.cc
@@ -1,4 +1,53 @@
 /*
+  2020/09/14
+  
+*/
+
+class Solution {
+private:
+    vector<string> res;
+    const vector<string> mp = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string &digits, int pos, string temp, vector<string> &res){
+        
+        if (pos > (int)digits.size()) return;
+        
+        if ( pos == (int)digits.size() ){
+            res.emplace_back(temp);
+            return;
+        } 
+        
+        
+        
+        for ( char c : mp[ digits[pos] - '0'] ){
+            
+            dfs(digits,pos+1, temp+c,res);
+        }
+        
+        return;
+    }
+    
+    
+    
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        if ( (int)digits.size() == 0 ) return res;
+        dfs(digits,0,"",res);
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+/*
   author: Tsubasa Takamuku
   date: 2020/08/07
   
