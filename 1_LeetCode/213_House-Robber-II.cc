90d49b4ea54d92d0ab1604751ab4d50350e58a5a
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-15 11:17:04 +0900

    Update 213_House-Robber-II.cc

--- 1_LeetCode/213_House-Robber-II.cc
+++ 1_LeetCode/213_House-Robber-II.cc
@@ -1,4 +1,39 @@
 /*
+    2020/09/15
+*/
+class Solution {
+private:
+    
+    int solve(vector<int> &nums, int left, int right){
+        int prev = 0;
+        int cur = 0;
+        
+        for (int i = left; i <= right; i++){
+            int temp = max(prev+nums[i],cur);
+            prev = cur;
+            cur = temp;
+        }
+        
+        return cur;
+    }
+public:
+    
+    // 前半: 0番目~ n-2番目まで
+    // 後半: 1番目~ n-1番目まで
+    int rob(vector<int>& nums) {
+        int n = (int) nums.size();
+        
+        if (n < 2) return n ? nums[0] : 0;
+        
+        return max(solve(nums,0,n-2), solve(nums,1,n-1));
+    }
+};
+
+
+
+
+
+/*
     cited from https://leetcode.com/problems/house-robber-ii/discuss/59921/9-lines-0ms-O(1)-Space-C%2B%2B-solution
 */
 
