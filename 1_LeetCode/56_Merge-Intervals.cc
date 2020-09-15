5369cf8c042357eabd805157e0cb402e105a21b4
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-15 10:14:45 +0900

    Update 56_Merge-Intervals.cc

--- 1_LeetCode/56_Merge-Intervals.cc
+++ 1_LeetCode/56_Merge-Intervals.cc
@@ -1,3 +1,36 @@
+/*
+    2020/09/15
+*/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        
+        int n = (int)intervals.size();
+        if ( n == 0 ) return res;
+        
+        //sort(intervals.begin(),intervals.end(), [](vector<int> a, vector<int> b){ return a[0] < b[0];});
+        
+        sort(intervals.begin(),intervals.end());
+        
+        res.emplace_back(intervals[0]);
+        
+        for (int i = 1; i < n; i++){
+            
+            if (res[res.size()-1][1] < intervals[i][0]) res.emplace_back(intervals[i]);
+            else{
+                res[res.size()-1][1] = max(res[res.size()-1][1],intervals[i][1]);
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
