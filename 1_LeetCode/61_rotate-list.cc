c2210c40a1a77c09a1af620a3d4b4584acb06d46
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-16 11:33:51 +0900

    Update 61_rotate-list.cc

--- 1_LeetCode/61_rotate-list.cc
+++ 1_LeetCode/61_rotate-list.cc
@@ -1,3 +1,56 @@
+/*
+    2020/09/16
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        if (head == nullptr) return head;
+        
+        ListNode *new_head = head;
+        ListNode *tail = head;
+        
+        // linked listのサイズを求める
+        int list_size = 1;
+        while (tail->next != nullptr){
+            list_size++;
+            tail = tail->next;
+        }
+        
+        // ring buffer状にする
+        tail->next = head;
+        
+        k %= list_size;
+        // 新しいtailの場所へ移動する
+        if (k > 0){
+            
+            for (int i = 0; i < (list_size - k); i++){
+                tail = tail->next;
+            }
+        }
+        
+        // k == 0は, originalと同じ状態である.
+        
+        new_head = tail->next;
+        tail->next = nullptr;
+        return new_head;
+        
+    }
+};
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
