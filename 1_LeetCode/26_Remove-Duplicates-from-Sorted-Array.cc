dfcf1615faa623e77b253a8ae01aec02427ef36c
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-16 10:36:48 +0900

    Update 26_Remove-Duplicates-from-Sorted-Array.cc

--- 1_LeetCode/26_Remove-Duplicates-from-Sorted-Array.cc
+++ 1_LeetCode/26_Remove-Duplicates-from-Sorted-Array.cc
@@ -1,3 +1,30 @@
+/*
+    2020-09-16
+*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = (int)nums.size();
+        
+        if (n == 0) return 0;
+        
+        int pos = 0; // [0,pos]の間はduplicateが除去されている
+        
+        for (int i = 1; i < n; i++){
+            if (nums[pos] != nums[i]){
+                pos++;
+                nums[pos] = nums[i];
+            }
+        }
+        
+        // [0,0,1,1,2,2,3]
+        // when finished, pos = 3, expected = 4
+        return pos+1;
+    }
+};
+
+
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
