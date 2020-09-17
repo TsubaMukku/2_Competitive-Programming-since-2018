69eb9de39d4e923d31264a95feaf44850dcfdf0d
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-17 11:09:46 +0900

    Update 510_Inorder-Successor-in-BST-II.cc

--- 1_LeetCode/510_Inorder-Successor-in-BST-II.cc
+++ 1_LeetCode/510_Inorder-Successor-in-BST-II.cc
@@ -1,3 +1,41 @@
+/*
+  2020/09/17
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* inorderSuccessor(Node* node) {
+        if (node == nullptr) return nullptr;
+        
+        if (node->right != nullptr){
+            node = node->right;
+            
+            while (node->left != nullptr) node = node->left;
+            return node;
+        }
+        
+        while (node->parent != nullptr && node == node->parent->right) node = node->parent;
+        return node->parent;
+    }
+};
+
+
+
+
+
+
+
 
 
 /*
