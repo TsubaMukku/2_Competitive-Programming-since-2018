fae7fb3e19a1f47c590d3f7341bba519215279c6
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-17 10:56:54 +0900

    Update 58_Length-of-Last-Word.cc

--- 1_LeetCode/58_Length-of-Last-Word.cc
+++ 1_LeetCode/58_Length-of-Last-Word.cc
@@ -1,3 +1,26 @@
+/*
+    2020/09/17 
+*/
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int pos = (int)s.size()-1;
+        
+        while (pos >= 0 && s[pos] == ' ') pos--;
+        
+        int cnt = 0;
+        
+        while (pos >= 0 && s[pos] != ' '){
+            pos--;
+            cnt++;
+        }
+
+        return cnt;
+    }
+};
+
+
+
 class Solution {
 public:
     int lengthOfLastWord(string s) {
