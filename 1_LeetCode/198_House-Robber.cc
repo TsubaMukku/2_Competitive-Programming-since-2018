e010a1e9e9ccf65b7794ae5dff13363119e7ba87
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-17 11:15:09 +0900

    Update 198_House-Robber.cc

--- 1_LeetCode/198_House-Robber.cc
+++ 1_LeetCode/198_House-Robber.cc
@@ -1,4 +1,31 @@
 /*
+    2020/09/17
+*/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = (int)nums.size();
+        
+        if (n == 0) return 0;
+        
+        vector<int> dp(n); // dp[i]: [0,i]個目の家までで得られるお金の最大値を記録する
+        dp[0] = nums[0];
+        if (n >= 2)dp[1] = max(dp[0],nums[1]);
+        
+        for (int i = 2; i < n; i++){
+            dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
+        }
+        
+        return dp[n-1];
+    }
+};
+
+
+
+
+
+/*
     2020/09/15
 */
 class Solution {
