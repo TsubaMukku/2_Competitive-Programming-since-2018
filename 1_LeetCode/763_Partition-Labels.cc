12f622e5ffe27e720c08a7f9bfd80cc48b9bcc08
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-20 09:20:44 +0900

    Update 763_Partition-Labels.cc

--- 1_LeetCode/763_Partition-Labels.cc
+++ 1_LeetCode/763_Partition-Labels.cc
@@ -1,3 +1,33 @@
+// 2020-09-20
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        int n = (int)intervals.size();
+        if (n == 0) return res;
+        sort(intervals.begin(),intervals.end());
+        
+        res.emplace_back(intervals[0]);
+        
+        //int first = intervals[0][0];
+        //int last = intervals[0][1];
+        
+        for (int i = 1; i < n; i++){
+            
+            if (res[(int)res.size()-1][1] < intervals[i][0]) res.emplace_back(intervals[i]);
+            else{
+                res[(int)res.size()-1][0] = min(res[(int)res.size()-1][0], intervals[i][0]);
+                res[(int)res.size()-1][1] = max(res[(int)res.size()-1][1], intervals[i][1]);
+                
+                
+            }
+            
+        }
+        return res;
+    }
+};
+
+
 /*
     2020-09-11
 */
