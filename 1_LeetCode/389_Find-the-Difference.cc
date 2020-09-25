dc839bcd0720416942eb1ab21db8ba315ba93247
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-09-25 18:59:32 +0900

    Update 389_Find-the-Difference.cc

--- 1_LeetCode/389_Find-the-Difference.cc
+++ 1_LeetCode/389_Find-the-Difference.cc
@@ -1,4 +1,20 @@
 
+// 2020-09-25
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n = (int)s.size();
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        for (int i = 0; i < n; i++){
+            if (s[i] != t[i]) return t[i];
+        }
+        
+        return t[t.size()-1];
+    }
+};
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
