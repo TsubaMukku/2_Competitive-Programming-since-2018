c7868ffbd1226f1561723c13d52599c53a320ac4
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-12-25 11:40:49 +0900

    Update 31_Next-Permutation.cc

--- 1_LeetCode/31_Next-Permutation.cc
+++ 1_LeetCode/31_Next-Permutation.cc
@@ -76,3 +76,28 @@ public:
         }
     }
 }; 
+
+
+// 2020-12-25
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.size() == 1) return;
+        
+        int i = nums.size()-2;
+        
+        while (i >= 0 && nums[i] >= nums[i+1]) i--;
+        
+        if (i < 0) {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        int j = nums.size()-1;
+        
+        while (j >= 0 && nums[i] >= nums[j]) j--;
+        swap(nums[i],nums[j]);
+        reverse(nums.begin()+i+1,nums.end());
+        return;
+    }
+};
