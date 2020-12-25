dfcdb092ceb389e4a29188b87f6d2099a2866eb9
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-12-25 20:31:01 +0900

    Update 556_Next_Greater_Element_III.cc

--- 1_LeetCode/556_Next_Greater_Element_III.cc
+++ 1_LeetCode/556_Next_Greater_Element_III.cc
@@ -23,32 +23,3 @@ public:
 };
 
 
-// TLE code
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* swapPairs(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
-        
-        ListNode *cur = head;
-        head = head->next;
-        
-        while (cur != nullptr  &&  cur->next != nullptr){
-            ListNode *keep = cur->next;
-            cur->next->next = cur;
-            cur->next = keep->next;
-            cur = cur->next;
-        }
-        
-        return head;
-    }
-};
