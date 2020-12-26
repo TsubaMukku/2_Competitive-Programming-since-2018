ac0fb18e661702f5a798b3da1333cf31219d77ef
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-12-26 10:08:33 +0900

    reviewed the problem

--- 1_LeetCode/498_Diagonal_Traverse.cc
+++ 1_LeetCode/498_Diagonal_Traverse.cc
@@ -1,4 +1,35 @@
 
+// 2020-12-26
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        
+        int m = matrix.size();
+        if (m == 0) return ans;
+        
+        int n = matrix[0].size();
+        
+        for (int i = 0; i < m + n -1; i++){
+            int row = i < n ? 0 : i - n + 1;
+            int col = i < n ? i : n - 1;
+            
+            vector<int> temp;
+            
+            while (row < m && col >= 0){
+                temp.emplace_back(matrix[row][col]);
+                row++;
+                col--;
+            }
+            
+            if (i % 2 == 0) reverse(temp.begin(),temp.end());
+            
+            for (auto x : temp) ans.emplace_back(x);
+        }
+        return ans;
+        
+    }
+};
 
 // 2020-12-25
 class Solution {
