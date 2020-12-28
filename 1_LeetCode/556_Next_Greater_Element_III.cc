24b0cf1d82fb14d361a479e73b8ac015a52d8eda
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2020-12-28 19:39:26 +0900

    updated the solution

--- 1_LeetCode/556_Next_Greater_Element_III.cc
+++ 1_LeetCode/556_Next_Greater_Element_III.cc
@@ -1,4 +1,28 @@
 
+// 2020-12-28
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+        
+        int i = s.size()-2;
+        
+        while (i >= 0 && s[i] >= s[i+1])i--;
+        
+        if (i < 0) return -1; // non decreasing
+        
+        int j = s.size()-1;
+        
+        while (j >= 0 && s[i] >= s[j])j--;
+        
+        swap(s[i],s[j]);
+        reverse(s.begin()+i+1,s.end());
+        
+        long long ans = stoll(s);
+        if (ans > INT_MAX) return -1;
+        else return (int) ans;
+    }
+};
 
 // 2020-12-26
 class Solution {
