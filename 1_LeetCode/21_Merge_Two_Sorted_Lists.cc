8ff48f5cb449e759c999e9b87bf13790fea8314e
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2021-01-05 16:54:54 +0900

    updated the file

--- 1_LeetCode/21_Merge_Two_Sorted_Lists.cc
+++ 1_LeetCode/21_Merge_Two_Sorted_Lists.cc
@@ -1,3 +1,60 @@
+
+// 2021/01/05
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        vector<int> tmp;
+        
+        ListNode *first = l1;
+        ListNode *second = l2;
+        
+        while (first != nullptr && second != nullptr){
+            if (first->val <= second->val){
+                tmp.emplace_back(first->val);
+                first = first->next;
+            }
+            else{
+                tmp.emplace_back(second->val);
+                second = second->next;
+            }
+        }
+        
+        while(first != nullptr){
+            tmp.emplace_back(first->val);
+            first = first->next;
+        }
+        
+        while (second != nullptr){
+            tmp.emplace_back(second->val);
+            second = second->next;
+        }
+        
+        if (tmp.size() == 0){
+            return l1;
+        }
+        ListNode *head = new ListNode(tmp[0]);
+        ListNode *ans = head;
+        for (int i = 1; i < tmp.size(); i++){
+            ans->next = new ListNode(tmp[i]);
+            ans = ans->next;
+        }
+        
+        ans->next = nullptr;
+        return head;
+    }
+};
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
