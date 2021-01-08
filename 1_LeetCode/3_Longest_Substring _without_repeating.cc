3bfdff00e30993e625fbfe080fad50052555946a
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2021-01-08 17:15:01 +0900

    update the file 3

--- 1_LeetCode/3_Longest_Substring _without_repeating.cc
+++ 1_LeetCode/3_Longest_Substring _without_repeating.cc
@@ -1,6 +1,26 @@
 
 
 
+// 2021/01/08　OK
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        
+        int ans = 0;
+        
+        vector<int> idx(128,-1); // その文字が最後に現れた場所を記録する.
+        for (int i = 0, j = 0; j < n; j++){
+            i = max(i,idx[s[j]]+1);
+            ans = max(ans,j-i+1);
+            idx[s[j]] = j;
+        }
+        
+        return ans;
+    }
+};
+
+
 
 // 2021/01/08　NG　code -> s == "   "のような場合に対応できていない
 class Solution {
