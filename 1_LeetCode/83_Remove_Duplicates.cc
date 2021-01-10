3fc153966c57c31dea92b5effe71a7e8d625c18c
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2021-01-10 20:04:09 +0900

    update 83

--- 1_LeetCode/83_Remove_Duplicates.cc
+++ 1_LeetCode/83_Remove_Duplicates.cc
@@ -1,5 +1,28 @@
 
 
+// 2021/01/10
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr) return head;
+        
+        ListNode *cur = head;
+        
+        while (cur != nullptr){
+            
+            if (cur->next != nullptr && cur->val == cur->next->val){
+                cur->next = cur->next->next;
+            }
+            else{
+                cur = cur->next;
+            }
+        }
+        
+        return head;
+    }
+};
+
+
 // 2021/01/08
 class Solution {
 public:
