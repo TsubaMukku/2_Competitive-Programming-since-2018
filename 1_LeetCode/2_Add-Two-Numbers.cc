56979749004b56e747d45382bf1a2f84480c4ee2
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2021-01-13 09:12:09 +0900

    revised the file 2

--- 1_LeetCode/2_Add-Two-Numbers.cc
+++ 1_LeetCode/2_Add-Two-Numbers.cc
@@ -1,3 +1,73 @@
+
+
+// 2021/01/13
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int tmp = l1->val + l2->val;
+        int res = tmp / 10;
+        
+        int q = tmp % 10;
+        
+        ListNode *ans = new ListNode(q);
+        ListNode *cur = ans;
+        
+        l1 = l1->next, l2 = l2->next;
+        
+        
+        while(l1 != nullptr && l2 != nullptr){
+            tmp = l1->val + l2->val;
+            tmp += res;
+            cur->next = new ListNode(tmp % 10);
+            cur = cur->next;
+            
+            res = tmp / 10;
+            l1 = l1->next, l2 = l2->next;
+        }
+        
+        while (l1 != nullptr){
+            tmp = l1->val;
+            tmp += res;
+            cur->next = new ListNode(tmp % 10);
+            cur = cur->next;
+            
+            res = tmp / 10;
+            l1 = l1->next;
+        }
+        
+        while (l2 != nullptr){
+            tmp = l2->val;
+            tmp += res;
+            cur->next = new ListNode(tmp % 10);
+            cur = cur->next;
+            
+            res = tmp / 10;
+            l2 = l2->next;
+        }
+        
+        if (res > 0){
+            cur->next = new ListNode(res);
+            cur = cur->next;
+            return ans;
+        }
+        else{
+            return ans;
+        }
+        
+    }
+};
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
