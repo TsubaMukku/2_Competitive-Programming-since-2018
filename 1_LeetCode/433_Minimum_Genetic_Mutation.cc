8f4141b36010ec9014d99b0409b17f1b0a9cb82a
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2021-01-14 12:46:33 +0900

    revisited the problem 433

--- 1_LeetCode/433_Minimum_Genetic_Mutation.cc
+++ 1_LeetCode/433_Minimum_Genetic_Mutation.cc
@@ -1,4 +1,46 @@
 
+// 2021/01/14
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set<string> check(bank.begin(),bank.end());
+        vector<char> word = {'A','T','G','C'};
+        if (!check.count(end)) return -1;
+        
+        queue<string> q;
+        q.push(start);
+        
+        int step = 0;
+        
+        while (q.size()){
+            step++;
+            int size = q.size();
+            
+            while (size--){
+                string w = q.front();
+                q.pop();
+                
+                for (int i = 0; i < w.size(); i++){
+                    char tmp = w[i];
+                    
+                    for (int j = 0; j < 4; j++){
+                        w[i] = word[j];
+                        
+                        if (w == end) return step;
+                        
+                        if (!check.count(w)) continue;
+                        check.erase(w);
+                        q.push(w);
+                    }
+                    w[i] = tmp;
+                }
+            }
+            
+        }
+        
+        return -1;
+    }
+};
 
 // 2021/01/10
 class Solution {
