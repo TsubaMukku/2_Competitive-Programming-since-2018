116034fa7d4abcaa65422036eff6b6bd44dbc87b
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2021-01-27 08:16:36 +0900

    update 1437

--- 1_LeetCode/1437.cc
+++ 1_LeetCode/1437.cc
@@ -1,5 +1,36 @@
 
 
+// 2021/01/27
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int i = 0;
+        int n = (int)nums.size();
+        
+        while (i < n && nums[i] == 0) i++;
+        i++;
+        
+        int cnt = 0;
+        while (i < n){
+            if (nums[i] == 1){
+                if (cnt >= k){
+                    cnt = 0;
+                    i++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                cnt++;
+                i++;
+            }
+        }
+        
+        return true;
+    }
+};
+
 // 2021/01/26
 // ref: https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/solution/
 
