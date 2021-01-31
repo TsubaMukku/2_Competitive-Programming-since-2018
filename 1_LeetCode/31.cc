c78c86137cc2f682e17594e607f81c0838475baa
Tsubasa Takamuku <52277348+TsubaMukku@users.noreply.github.com>
2021-02-01 08:49:58 +0900

    update and rename the file31

--- 1_LeetCode/31.cc
+++ 1_LeetCode/31.cc
@@ -1,3 +1,30 @@
+
+
+// 2021/02/01
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = (int)nums.size();
+        if (n == 1) return;
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i+1])i--;
+        
+        if (i == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        int j = n - 1;
+        while (j >= 0 && nums[i] >= nums[j])j--;
+        swap(nums[i],nums[j]);
+        reverse(nums.begin()+i+1,nums.end());
+        return;
+    }
+};
+
+
+
+
 // solution 1:
 
 /*
