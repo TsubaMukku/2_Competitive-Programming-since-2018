eaf27408412b758ae8a336bd1bed6682b2ac66c4
TsubaMukku <52277348+TsubaMukku@users.noreply.github.com>
2021-02-10 09:23:50 +0900

    create 938

---
+++ 1_LeetCode/938.cc
@@ -0,0 +1,26 @@
+
+// 2021/02/10
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int total = 0;
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root){
+            rangeSumBST(root->right,low,high);
+            if (low <= root->val && root->val <= high) total += root->val;
+            rangeSumBST(root->left,low,high);
+        }
+        return total;
+    }
+};
