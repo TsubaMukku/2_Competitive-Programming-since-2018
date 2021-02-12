a0ec273797194484305762c7af79366ae53151db
TsubaMukku <52277348+TsubaMukku@users.noreply.github.com>
2021-02-12 09:53:24 +0900

    update 71

--- 1_LeetCode/71.cc
+++ 1_LeetCode/71.cc
@@ -1,5 +1,59 @@
 
 
+// 2021/02/12
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        stringstream ss(path); // 文字列分割の準備をする
+        string s; // /の手前までの文字列を収納するための変数を準備する
+        stack<string> st; // 答えを得るための部分文字列を収納する
+        
+        
+        // ssの中から/区切りで文字列を取得してsへ格納する
+        while (getline(ss,s,'/')){
+            
+            if (s == "") continue; // //の場合
+            else if (s == ".") continue;
+            else if (s != "..") st.push(s);
+            else{
+                // s == ".."の場合である
+                if (st.size()) st.pop();
+            }
+        }
+        
+        if (st.size() == 0) return "/";
+        string ans = "";
+        
+        while (st.size()){
+            string tmp = st.top();
+            st.pop();
+            
+            ans = "/" + tmp + ans;
+        }
+        
+        return ans;
+    }
+};
+
+/*
+ぜその解法が思い付かなかったのか
+・特殊な操作（pop）が必要になるのは".."の場合のみという点に着目できなかった
+・・・具体例からそのポイントを抽出できなかった
+
+
+どういう思考が足りなかったのか
+具体例を通した状況把握、場合分けの検討により
+・//の中身を取ってきて、それぞれに対して場合分けを行う、という方針を立てられなかった
+・・・・//の中身を取る方法（STL）を知らなかったため、その発想ができなくて当然だと思った
+
+
+
+もし必要とされる思考法が足りていたとしたら解法を思い付けたのか
+・yes
+
+*/
+
 // 2021/02/11
 class Solution {
 public:
