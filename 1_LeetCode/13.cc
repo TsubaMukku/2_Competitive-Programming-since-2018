bbe04035af8a033cf01e3d2b31b6e1a9a16a9985
TsubaMukku <52277348+TsubaMukku@users.noreply.github.com>
2021-02-22 11:52:48 +0900

    update 13

--- 1_LeetCode/13.cc
+++ 1_LeetCode/13.cc
@@ -1,5 +1,41 @@
 
 
+// 2021/02/22
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = (int)s.size();
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++){
+            
+            if (s[i] == 'V') ans += 5;
+            if (s[i] == 'L') ans += 50;
+            if (s[i] == 'D') ans += 500;
+            if (s[i] == 'M') ans += 1000;
+            
+            if (s[i] == 'I'){
+                if (i <= n-2 && s[i+1] == 'V') ans--;
+                else if (i <= n-2 && s[i+1] == 'X') ans--;
+                else ans++;
+            }
+            if (s[i] == 'X'){
+                if (i <= n-2 && s[i+1] == 'L') ans -= 10;
+                else if (i <= n-2 && s[i+1] == 'C') ans -= 10;
+                else ans += 10;
+            }
+            if (s[i] == 'C'){
+                if (i <= n-2 && s[i+1] == 'D') ans -= 100;
+                else if (i <= n-2 && s[i+1] == 'M') ans -= 100;
+                else ans += 100;
+            }
+            
+        }
+        
+        return ans;
+    }
+};
+
 // 2020-09-24
 class Solution {
 public:
